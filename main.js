ac4188b67efb72489667af0ef71ba77eddb975e3
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-02-23 19:38:56 -0500

    added while loop

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 window.onload = init;
 
 function init() {
-    document.querySelector('#input')
-        .addEventListener('keydown', handleTyping);
+  document.querySelector("#input").addEventListener("keyup", handleTyping);
 }
 
 function handleTyping(event) {
-    let userInput = event.target.value;
-    let answer = userInput;
-    
-    document.querySelector('#changeme').innerText = answer;
+  let userInput = event.target.value;
+  let answer = "";
+  let i = 0;
+
+  let emptyString = " ";
+  while (i < userInput.length){
+      if (i === 0 || userInput[i-1] === emptyString) {
+          answer = answer + userInput[i].toUpperCase();
+      } else {
+          answer = answer + userInput[i];
+      }
+      i++
+
+  }
+//   while (i < answer.length) {
+//     if (i === 0) {
+//       console.log(`the letter: ${answer[i].toUpperCase()}`);
+//       answer[i] = answer[i].toUpperCase();
+//       console.log("first letter", answer);
+//     }
+//     if (answer[i] === emptyString) {
+//       console.log(`the letter: ${answer[i + 1].toUpperCase()}`);
+//       answer[i + 1] = answer[i + 1].toUpperCase();
+//       console.log("afternspace", answer);
+//     }
+//     i++;
+
+
+  
+  console.log(answer);
+
+  document.querySelector("#changeme").innerText = answer;
 }
