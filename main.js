63b6313fc95eb5d96cdde88fe4e5b6a8f76eebd0
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-02-26 16:04:20 -0500

    created function for input2

--- main.js
+++ main.js
@@ -2,50 +2,54 @@ window.onload = init;
 
 function init() {
   document.querySelector("#input").addEventListener("keyup", handleTyping);
+
+  document.querySelector("#input2").addEventListener("keyup", handleTyping2);
 }
 
 function handleTyping(event) {
   let userInput = event.target.value;
-  // userInput =
+  // userInput = ** form input(where you are typing)
   let answer = "";
-  // answer = empty string
+  // answer = empty string ** output
   let i = 0;
   // value of index = 0 [starting position 0]
-  let emptyString = " ";
-  // value of emptyString = space
+  let space = " ";
+  // value of space = space
   while (i < userInput.length) {
     // index is less than the indexed length of userInput
-    if (i === 0 || userInput[i - 1] === emptyString) {
+    if (i === 0 || userInput[i - 1] === space) {
       // check if [i] is equal to zero
       // or
-      // check if userInput's previous index is an emptyString with [i-1]
+      // check if userInput's previous index is an empty string with [i-1]
       answer = answer + userInput[i].toUpperCase();
-      // current 'answer' is an emptyString
-      // =
+      // current 'answer' is an empty string
+      // = 
       // answer 'which is emptyString' +  userInput's index of 0 and every index after empty string 
-      //**this will run the loop through each index to check if true, if true this will print toUpperCase() for that index */
+      //**this will run the loop through each index to check if true, if true this will print the index of 0 or after an emptry stringtoUpperCase() for that index */
     } else {
       answer = answer + userInput[i];
       // if line 18 runs false line 28 will execute 
       //"" = "" + userInput's lowercases
     }
     i++;
+
+    
     // bumps one up each index to run checks
   }
-  //   while (i < answer.length) {
-  //     if (i === 0) {
-  //       console.log(`the letter: ${answer[i].toUpperCase()}`);
-  //       answer[i] = answer[i].toUpperCase();
-  //       console.log("first letter", answer);
-  //     }
-  //     if (answer[i] === emptyString) {
-  //       console.log(`the letter: ${answer[i + 1].toUpperCase()}`);
-  //       answer[i + 1] = answer[i + 1].toUpperCase();
-  //       console.log("afternspace", answer);
-  //     }
-  //     i++;
 
   console.log(answer);
 
   document.querySelector("#changeme").innerText = answer;
 }
+
+function handleTyping2(event) {
+  let userInput2 = event.target.value;
+  let answer = "";
+
+  answer = answer + userInput2;
+
+
+  console.log(answer);
+
+  document.querySelector("#changeme2").innerText = answer;
+} 
